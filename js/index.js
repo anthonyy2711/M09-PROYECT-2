1b31d4efb5036b9c7fd139dbf71ae377f6a8d800
anthonyy2711 <amreyesnarvaez@gmail.com>
2022-12-20 18:48:45 +0100

    navidad

--- js/index.js
+++ js/index.js
@@ -1,16 +1,30 @@
         
 $(document).ready(function() {
+    $(window).scroll(function() {
+        var hT = $('#div-counter').offset().top,
+            hH = $('#div-counter').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+         console.log((hT-wH) , wS);
+        if (wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)){
+            $('.counter').each(function () {
+                $(this).prop('Counter',0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 4000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                    
+                });
+                
+                });
+               
+        }
+        
+     });
 
-    $('.counter').each(function () {
-$(this).prop('Counter',0).animate({
-    Counter: $(this).text()
-}, {
-    duration: 4000,
-    easing: 'swing',
-    step: function (now) {
-        $(this).text(Math.ceil(now));
-    }
-});
-});
 
+     return false;
 });  
